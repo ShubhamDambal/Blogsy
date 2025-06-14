56a66b76ea03f8b4ca10be4beb5867daaf5f77ab
Shubham Dambal <shubhamdambal@gmail.com>
2025-06-14 14:53:19 +0530

    project created and env variables set. Appwrite configurations are made.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.REACT_APP_APPWRITE_URL);
+  
+
+  return (
+    <h1>A Blog app with Appwrite!</h1>
+  )
+}
+
+export default App
