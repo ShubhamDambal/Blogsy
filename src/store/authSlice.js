e526af071bcb577a790a6c309be527b207fe3d90
Shubham Dambal <shubhamdambal@gmail.com>
2025-06-15 22:12:32 +0530

    created store. Created reducer for auth.

---
+++ src/store/authSlice.js
@@ -0,0 +1,28 @@
+//authSlice is for, we are asking to this that user is authenticated or not
+
+import { createSlice } from "@reduxjs/toolkit";
+
+//initial state
+const initialState = {  
+  status: false,   //initially status is false
+  userData: null   //and there is no user
+}
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {   //stores "key: func()" pairs
+    login: (state, action) => {
+      state.status = true;
+      state.userData = action.payload.userData;
+    },
+    logout: (state) => {
+      state.status = false;
+      state.userData = null;
+    }
+  }
+})
+
+export const {login, logout} = authSlice.actions;
+
+export default authSlice.reducer;
