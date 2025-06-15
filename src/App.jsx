b18036be829379f2368e93d66deda51493807f7c
Shubham Dambal <shubhamdambal@gmail.com>
2025-06-15 23:46:28 +0530

    handled loading using store.  Added tailwind css.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
 import './App.css'
+import { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import {login, logout} from './store/authSlice'
+import { Footer, Header } from './components'
 
 function App() {
-  return (
-    <h1>A Blog app with Appwrite!</h1>
-  )
+  //create loading bcz some time needed to fetch data fro appwrite
+  const [loading, setLoading] = useState(true)  //initially loading is true
+  const dispatch = useDispatch()  //getting current user from store
+
+  //when app start using useEffect() we'll figure out user is logged in or not
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())  //if no userData then logout
+      }
+    })
+    .finally(() => setLoading(false))  //always runs  
+  }, [])
+
+  //conditional rendering(render iff user is logged in)
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          {/*<Outlet />*/}
+          TODO
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
