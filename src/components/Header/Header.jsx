a70140f765bf1ba49f4761ed9445cc4d738738af
Shubham Dambal <shubhamdambal@gmail.com>
2025-06-16 21:21:32 +0530

    added container component. updated Footer. ...Updating Header by creating Logout component..

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,45 @@
 import React from 'react'
+import { Container, Logo, LogoutBtn } from '../index'
+import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
-export const Header = () => {
+function Header() { 
+  //check status of user(logged in or not)
+  const authStatus = useSelector((state) => state.auth.status)
+  const navigate = useNavigate()
+
+  const naItems = [
+    {
+      name: 'Home',
+      slug: "/",
+      active: true
+    },
+    {
+      name: "Login",
+      slug: "/login",
+      active: !authStatus  //bcz if user is loggen in then do not show login & signup button
+    },
+    {
+      name: "Signup",
+      slug: "/signup",
+      active: !authStatus
+    },
+    {
+      name: "All posts",
+      slug: "/all-posts",
+      active: authStatus
+    },
+    {
+      name: "Add post",
+      slug: "/add-post",
+      active: authStatus
+    }
+  ]
+
   return (
-    <div>Header</div>
+    <header></header>
   )
 }
+
+export default Header
---
+++ src/components/Header/LogoutBtn.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import authService from '../../appwrite/auth'
+import {logout} from '../../store/authSlice'
+
+function LogoutBtn() {
+  const dispatch = useDispatch();
+  const logoutHandler = () => {
+    //authService has logout as promise so we can use .then()
+    authService.logout().then(() => {
+      dispatch(logout());  //(authSlice has method logout())dispatch to store such that state should be up to date
+    })
+  }
+
+  return (
+    <button className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'>Logout</button>
+  )
+}
+
+export default LogoutBtn
