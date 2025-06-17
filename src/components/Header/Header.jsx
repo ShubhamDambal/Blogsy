4d271155ef61afd08634b9fcf8ca37dcce1836ad
Shubham Dambal <shubhamdambal@gmail.com>
2025-06-17 20:10:04 +0530

    Created Button , Input component. Modified Header component.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,7 +9,9 @@ function Header() {
   const authStatus = useSelector((state) => state.auth.status)
   const navigate = useNavigate()
 
-  const naItems = [
+  //things to shows on header
+  //why created like this?(just production grade things. Makes easy to future updates)
+  const navItems = [
     {
       name: 'Home',
       slug: "/",
@@ -18,7 +20,7 @@ function Header() {
     {
       name: "Login",
       slug: "/login",
-      active: !authStatus  //bcz if user is loggen in then do not show login & signup button
+      active: !authStatus  //bcz if user is logged in then do not show login & signup button
     },
     {
       name: "Signup",
@@ -38,7 +40,35 @@ function Header() {
   ]
 
   return (
-    <header></header>
+    <header className='py-3 shadow bg-gray-500'>
+      <Container>
+        <nav className='flex'>
+          <div className='mr-4'>
+            <Link to='/'>
+              <Logo width='70px'/>
+            </Link>
+          </div>
+          <ul className='flex ml-auto'>
+            {navItems.map((item) => 
+              item.active ? (
+                <li key={item.name}>  
+                  <button 
+                    onClick={() => navigate(item.slug)} 
+                    className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'>
+                    {item.name}
+                  </button>
+                </li>
+              ) : null )}
+
+              {authStatus && (
+                <li>
+                  <LogoutBtn />
+                </li>
+              )}
+          </ul>
+        </nav>
+      </Container>
+    </header>
   )
 }
 
---
+++ src/components/Input.jsx
@@ -0,0 +1,32 @@
+import React, {useId} from 'react'
+
+const Input = React.forwardRef(function Input({
+  lable,
+  type = "text",
+  className = '',
+  ...props
+}, ref){
+
+  const id = useId()  //for generating unique id's
+  return (
+    <div className='w-full'>
+      {
+      lable && 
+      <lable 
+      className='inline-block mb-1 pl-1'
+      htmlFor={id}>
+        {lable}
+      </lable>
+      }
+      <input 
+      type={type} 
+      className={`${className}`}
+      ref={ref}   //gives reference to the parent component. So from this reference we get to know on which <input> to apply onClick(), etc events. Thus we've used forwardRef().
+      {...props}
+      id={id}
+      />
+    </div>
+  )
+})
+
+export default Input
